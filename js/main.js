ccc83b4197449c3c8c0a8caa608511a98cf479dc
Thanh <thanhnguyen21294@gmail.com>
2019-09-16 21:39:03 +0700

    x

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload = init;
+
+function init(){
+    let inputDom = document.getElementById("input-firstname")
+    // inputDom.style.background = "gray";
+    inputDom.onclick = function(){
+        console.log("input on click");
+        
+    }
+}
+
