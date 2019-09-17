f461e0bae82c77dd09f2d356770e053dc0687f99
Thanh <thanhnguyen21294@gmail.com>
2019-09-17 14:21:01 +0700

    x

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-window.onload = init;
+// window.onload = init;
 
-function init(){
-    let inputDom = document.getElementById("input-firstname")
-    // inputDom.style.background = "gray";
-    inputDom.onclick = function(){
-        console.log("input on click");
+// function init(){
+//     let inputDom = document.getElementById("input-firstname")
+//     // inputDom.style.background = "gray";
+//     inputDom.onclick = function(){
+//         console.log("input on click");
         
-    }
-}
+//     }
+// }
 
